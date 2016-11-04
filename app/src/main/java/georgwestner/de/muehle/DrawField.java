cfdcd7725f4015aedf62dcea71a99e581a5ceace
Georg-W <georg.westner@googlemail.com>
2016-11-04 20:56:06 +0100

    layout complete

--- app/src/main/java/georgwestner/de/muehle/DrawField.java
+++ app/src/main/java/georgwestner/de/muehle/DrawField.java
@@ -43,18 +43,24 @@ public class DrawField extends View
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        int space = 350;
+
+
         Rect outer = new Rect();
         outer.set(10, 10,canvas.getWidth()-10,canvas.getHeight()-10);
         Rect middle = new Rect();
-        middle.set(150, 150,canvas.getWidth()-150,canvas.getHeight()-150);
+        middle.set(175, 175,canvas.getWidth()-175,canvas.getHeight()-175);
         Rect inner = new Rect();
-        inner.set(300, 300,canvas.getWidth()-300,canvas.getHeight()-300);
+        inner.set(350, 350,canvas.getWidth()-350,canvas.getHeight()-350);
 
 
-        canvas.drawLine(0, canvas.getHeight()/2, 300, canvas.getHeight()/2, outlinePaint);
-        canvas.drawLine(canvas.getWidth()-300, canvas.getHeight()/2, canvas.getWidth(), canvas.getHeight()/2, outlinePaint);
-        canvas.drawLine(canvas.getWidth()/2, 0,canvas.getWidth()/2, 300, outlinePaint);
-        canvas.drawLine(canvas.getWidth()/2, canvas.getHeight()-300, canvas.getWidth()/2, canvas.getHeight(), outlinePaint);
+
+
+        canvas.drawLine(0, canvas.getHeight()/2, space, canvas.getHeight()/2, outlinePaint);
+        canvas.drawLine(canvas.getWidth()-space, canvas.getHeight()/2, canvas.getWidth(), canvas.getHeight()/2, outlinePaint);
+        canvas.drawLine(canvas.getWidth()/2, 0,canvas.getWidth()/2, space, outlinePaint);
+        canvas.drawLine(canvas.getWidth()/2, canvas.getHeight()-space, canvas.getWidth()/2, canvas.getHeight(), outlinePaint);
         canvas.drawRect(outer, outlinePaint);
         canvas.drawRect(middle, outlinePaint);
         canvas.drawRect(inner, outlinePaint);
--- app/src/main/java/georgwestner/de/muehle/MainActivity.java
+++ app/src/main/java/georgwestner/de/muehle/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawField drawField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawField = new DrawField(this);
+
+
     }
 
 }
