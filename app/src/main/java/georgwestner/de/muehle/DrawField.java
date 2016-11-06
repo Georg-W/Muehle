b01b53c36b7a53e23e1e9d5f2dac85693c130347
ruby -v <georg.westner@googlemail.com>
2016-11-06 21:06:39 +0100

    changed layout added onMeasure

--- app/src/main/java/georgwestner/de/muehle/DrawField.java
+++ app/src/main/java/georgwestner/de/muehle/DrawField.java
@@ -17,6 +17,7 @@ public class DrawField extends View
 {
 
     final Paint outlinePaint;
+    final Paint fields;
 
     {
         outlinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -24,6 +25,13 @@ public class DrawField extends View
         outlinePaint.setStyle(Paint.Style.STROKE);
         outlinePaint.setColor(Color.BLACK);
         outlinePaint.setStrokeWidth(20);
+    }
+
+    {
+        fields = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fields.setAntiAlias(true);
+        fields.setStyle(Paint.Style.FILL);
+        fields.setColor(Color.BLACK);
     }
 
     public DrawField(Context context) {
@@ -39,32 +47,49 @@ public class DrawField extends View
     }
 
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int size = width > height ? height : width;
+        setMeasuredDimension(size, size);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
         int space = 350;
+        int outlinemargin =30;
 
 
         Rect outer = new Rect();
-        outer.set(10, 10,canvas.getWidth()-10,canvas.getHeight()-10);
+        outer.set(outlinemargin, outlinemargin,canvas.getWidth()-outlinemargin,canvas.getHeight()-outlinemargin);
         Rect middle = new Rect();
-        middle.set(175, 175,canvas.getWidth()-175,canvas.getHeight()-175);
+        middle.set(200, 200,canvas.getWidth()-200,canvas.getHeight()-200);
         Rect inner = new Rect();
         inner.set(350, 350,canvas.getWidth()-350,canvas.getHeight()-350);
 
 
 
 
-        canvas.drawLine(0, canvas.getHeight()/2, space, canvas.getHeight()/2, outlinePaint);
-        canvas.drawLine(canvas.getWidth()-space, canvas.getHeight()/2, canvas.getWidth(), canvas.getHeight()/2, outlinePaint);
-        canvas.drawLine(canvas.getWidth()/2, 0,canvas.getWidth()/2, space, outlinePaint);
-        canvas.drawLine(canvas.getWidth()/2, canvas.getHeight()-space, canvas.getWidth()/2, canvas.getHeight(), outlinePaint);
+        canvas.drawLine(outlinemargin, canvas.getHeight()/2, space, canvas.getHeight()/2, outlinePaint); //1st left to right
+        canvas.drawLine(canvas.getWidth()-space, canvas.getHeight()/2, canvas.getWidth()-outlinemargin, canvas.getHeight()/2, outlinePaint); //2nd left to right
+        canvas.drawLine(canvas.getWidth()/2, outlinemargin,canvas.getWidth()/2, space, outlinePaint); //1st top to bottom
+        canvas.drawLine(canvas.getWidth()/2, canvas.getHeight()-space, canvas.getWidth()/2, canvas.getHeight()-outlinemargin, outlinePaint); //2nd top to bottom
         canvas.drawRect(outer, outlinePaint);
         canvas.drawRect(middle, outlinePaint);
         canvas.drawRect(inner, outlinePaint);
         Log.d("test", "got created");
+
+
+        int circleNumbers = 24;
+
+        for (int i=0; i<24; i++){
+
+            canvas.drawCircle(30,30,30,fields);
+
+        }
 
 
 
